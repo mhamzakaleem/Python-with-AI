3bf4a72d1026733f966ced2a37f508df5da822f2
MUHAMMAD HAMZA KALEEM (mhamzakaleem) <mhamzakaleem2003@gmail.com>
2026-05-01 00:54:37 +0500

    add Week01 directiry with data_types.py file which have simple data types integer, string, float, boolean.

---
+++ Week01/data_types.py
@@ -0,0 +1,15 @@
+age = 22
+print(age)
+print(type(age))
+
+name="Hamza"
+print(name)
+print(type(name))
+
+num=13.4
+print(num)
+print(type(num))
+
+is_passed=True
+print(is_passed)
+print(type(is_passed))
