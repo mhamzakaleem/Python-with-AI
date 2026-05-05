67d000de08ecc9380ce8a5ddbae552222880928b
MUHAMMAD HAMZA KALEEM (mhamzakaleem) <mhamzakaleem2003@gmail.com>
2026-05-05 23:46:54 +0500

    Write practice code on data_structures.py file

---
+++ Week01/data_structures.py
@@ -0,0 +1,16 @@
+# List
+
+names=["Hamza", 3,"Kaleem"]
+print(names)
+print(names[1])
+
+# Tuples
+
+fruits=("Apple",3,'Orange')
+print(fruits)
+print(fruits[2])
+
+# Sets
+animals= {"Muhammad","Hamza", 3}
+print(animals)
+# print(animals[1])  This is wrong because list not support orderd
